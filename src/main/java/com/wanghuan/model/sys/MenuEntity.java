b56754fc8b63eab35e23fad993b6390113c47eaf
wangh <wangh@shanshuiyun.cn>
2017-11-27 18:15:05 +0800

    ing...

--- src/main/java/com/wanghuan/model/sys/MenuEntity.java
+++ src/main/java/com/wanghuan/model/sys/MenuEntity.java
@@ -1,12 +1,36 @@
 package com.wanghuan.model.sys;
 
 public class MenuEntity {
+	/**
+	 * 
+	 */
 	private int id;
+	/**
+	 * 
+	 */
 	private String name;
+	/**
+	 * 
+	 */
 	private String url;
+	/**
+	 * 
+	 */
 	private int parentId;
+	/**
+	 * 
+	 */
 	private int sort;
+	/**
+	 * 
+	 */
 	private String modules;
+	/**
+	 * 
+	 */
 	private String remark;
+	/**
+	 * 图标
+	 */
 	private String icon;
 }
